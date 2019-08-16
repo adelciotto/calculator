c9d595cf5196cd68244c348e12c7b3b28c37706e
adelciotto <adelciotto@zendesk.com>
2019-08-16 10:46:50 +1000

    ruby gem scaffold

---
+++ test/calculator_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class CalculatorTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Calculator::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
